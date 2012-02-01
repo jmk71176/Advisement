e943bd460fe80897182e7718911222be66d3e507
nanofied <hello@nanofied.com>
2012-01-31 19:11:52 -0500

    Added bootstrap and dashboard

---
+++ application/views/dashboard.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Advisement</title>
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link href="/css/bootstrap.css" rel="stylesheet" type="text/css">
+		<link href="/css/extra.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<a class="brand" href="#">Advisement</a>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="content">
+				<h1>Dashboard</h1>
+				<p>This application is currently under development, check back soon.</p>
+			</div>
+		</div>
+	</body>
+</html>
